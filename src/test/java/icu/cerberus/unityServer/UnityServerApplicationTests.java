e5699461d3bc76e0c8d7586f7b788b4a88ba9305
Cerberus Dante <dannydante1564@gmail.com>
2023-01-13 23:51:09 +0800

    init: 初始化仓库，添加配置

---
+++ src/test/java/icu/cerberus/unityServer/UnityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.cerberus.unityServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnityServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
